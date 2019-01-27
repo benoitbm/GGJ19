b2086e853a02a9fe5a4ca02bf61705f03da33a82
Benoit Boyer <contact@benoitbm.fr>
2019-01-26 20:20:40 -0500

    Phone checking
    And still controller friendly

---
+++ Assets/Scripts/phone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class phone : MonoBehaviour
+{
+    bool isHeld = true;
+
+    Rigidbody rb;
+    BoxCollider m_Collider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        m_Collider = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isHeld)
+        {
+            m_Collider.enabled = false;
+            rb.Sleep();
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else
+        {
+            m_Collider.enabled = true;
+            rb.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    public bool Hold
+    {
+        get { return isHeld; }
+        set { isHeld = Hold; }
+    }
+}
